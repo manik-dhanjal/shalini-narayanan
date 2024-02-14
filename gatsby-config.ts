169357a078529b9a2324ddba32758dab4c93ed5e
Manik Dhanjal <mdhanja1@ford.com>
2024-02-15 02:12:57 +0530

    nav bar created for desktop

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,15 +1,38 @@
-import type { GatsbyConfig } from "gatsby";
+import type { GatsbyConfig } from 'gatsby';
 
 const config: GatsbyConfig = {
   siteMetadata: {
     title: `Shalini Narayanan`,
-    siteUrl: `https://www.yourdomain.tld`
+    siteUrl: `https://shalininarayanan.in`,
   },
   // More easily incorporate content into your pages through automatic TypeScript type generation and better GraphQL IntelliSense.
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: ["gatsby-plugin-styled-components"]
+  plugins: [
+    'gatsby-plugin-styled-components',
+    `gatsby-plugin-typescript`,
+    `gatsby-plugin-image`,
+    `gatsby-plugin-sharp`,
+    `gatsby-transformer-sharp`, // Needed for dynamic images
+    {
+      resolve: `gatsby-omni-font-loader`,
+      options: {
+        enableListener: true,
+        preconnect: [`https://fonts.googleapis.com`, `https://fonts.gstatic.com`],
+        web: [
+          {
+            name: `lora`,
+            file: `https://fonts.googleapis.com/css2?family=Lora:ital,wght@0,400..700;1,400..700&display=swap`,
+          },
+          {
+            name: 'lato',
+            file: 'https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Lora:ital,wght@0,400..700;1,400..700&display=swap',
+          },
+        ],
+      },
+    },
+  ],
 };
 
 export default config;
