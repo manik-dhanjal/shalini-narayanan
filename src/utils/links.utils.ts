6c5040d3bdd1a8a90e3009d43a07087a19ff3479
Manik Dhanjal <mdhanja1@ford.com>
2024-04-11 16:17:37 +0530

    links util added

---
+++ src/utils/links.utils.ts
@@ -0,0 +1,16 @@
+export const downloadFileFromUrl = (url: string, defaultFileName: string = 'shalini-naryanan') => {
+  const fileName = url.split('/').pop() || defaultFileName;
+
+  const aTag = document.createElement('a');
+  aTag.href = url;
+  aTag.setAttribute('download', fileName);
+  aTag.setAttribute('target', '_blank');
+  document.body.appendChild(aTag);
+  aTag.click();
+  console.log(aTag);
+  aTag.remove();
+};
+
+export const openLinkInNewTab = (url: string) => {
+  window.open(url, '_blank')?.focus();
+};
